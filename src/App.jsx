12f5784e8d9e722f7fa7ce8d98f7375812808641
Saikat Roy <saikatroy007.skylark@gmail.com>
2025-11-30 19:23:19 +0530

    Initial commit message File Structure created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-slate-900 text-white text-9xl font-bold font-[Roboto] flex items-center justify-center'>
+      This is the App
+    </div>
+  )
+}
+
+export default App
